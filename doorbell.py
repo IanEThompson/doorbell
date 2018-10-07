075ba348e08dae891fea1694137b7d12dfae8f4e
Ian Thompson <ianmelandkids@gmail.com>
2018-10-07 17:05:12 +1000

    fixed button order

--- doorbell.py
+++ doorbell.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import serial
 import time
 import pygame
@@ -11,15 +13,23 @@ def playAudio(button):
     
     #choose which mp3 to play
     if button == btn[0]:
-        file = '/home/pi/Music/DrivingInMyCar.mp3'
+        file = '/home/pi/Music/StarWars.mp3'
     elif button == btn[1]:
-        file = '/home/pi/Music/BennyHill.mp3'
+        file = '/home/pi/Music/PeriodicTableSong.mp3'
     elif button == btn[2]:
         file = '/home/pi/Music/SliceOfHeaven.mp3'
     elif button == btn[3]:
-        file = '/home/pi/Music/StarWars.mp3'
+        file = '/home/pi/Music/RuleBrittania.mp3'
     elif button == btn[4]:
         file = '/home/pi/Music/WipeOut.mp3'
+    elif button == btn[5]:
+        file = '/home/pi/Music/BabyShark.mp3'
+    elif button == btn[6]:
+        file = '/home/pi/Music/Rawhide.mp3'
+    elif button == btn[7]:
+        file = '/home/pi/Music/BennyHill.mp3'
+    elif button == btn[8]:
+        file = '/home/pi/Music/DrivingInMyCar.mp3'
     else:
         return None
     
@@ -33,7 +43,8 @@ pygame.mixer.init()
 
 #create button instances from button[0] to button[11]
 btn = []
-buttonPin = [2,3,4,17,27,22,10,9,11,5,6,13]
+#buttonPin = [2,3,4,17,27,22,10,9,11,5,6,13]
+buttonPin = [2,3,4,17,27,22,13,6,5,11,9,10]
 for i in range(0,12):
     x = Button(buttonPin[i])
     btn.append(x)
